536fc1fecc7d37779e009ed2acadabc34c341c6c
Gumerov1508 <gumerovajn@gmail.com>
2022-07-05 15:37:27 +0300

    cron 15:45

--- src/main/java/org/example/NotificationBot/service/EventService.java
+++ src/main/java/org/example/NotificationBot/service/EventService.java
@@ -31,7 +31,7 @@ public class EventService {
     }
 
 
-    @Scheduled(cron = "0 0 0 * * *" , zone="Asia/Yekaterinburg")
+    @Scheduled(cron = "0 45 15 * * *" , zone="Asia/Yekaterinburg")
     private void eventService() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
