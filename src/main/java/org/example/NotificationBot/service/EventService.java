c6a954354a16852816fee90a3e9ad363c718a0dd
Gumerov1508 <gumerovajn@gmail.com>
2022-07-07 20:05:43 +0300

    idkwtfigo

--- src/main/java/org/example/NotificationBot/service/EventService.java
+++ src/main/java/org/example/NotificationBot/service/EventService.java
@@ -31,7 +31,7 @@ public class EventService {
     }
 
 
-    @Scheduled(cron = "0 01 00 * * *" , zone="Asia/Yekaterinburg")
+    @Scheduled(cron = "0 0 0 * * *" , zone="Asia/Yekaterinburg")
     private void eventService() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
